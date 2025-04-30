c8cb7975e1197052e7910c2be9b09a3e81b31006
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-04-30 21:30:02 +0100

    implement basic concurrency with user channel and goroutine

--- Concurrency/example.go
+++ Concurrency/example.go
@@ -1,1 +1,48 @@
 package concurrency
+
+import (
+	"fmt"
+	"time"
+)
+
+type Users struct {
+	Name string
+	Age  int
+}
+
+func Basic_Concurrency() {
+	// Make a channel with type `Users` and size `10`
+	ch := make(chan Users, 10)
+
+	// Slice of users with name and age
+	users := []Users{
+		{Name: "Alice", Age: 30},
+		{Name: "Bob", Age: 25},
+		{Name: "Charlie", Age: 35},
+		{Name: "Dave", Age: 28},
+		{Name: "Eve", Age: 22},
+	}
+
+	// Run an anonymous function in a goroutine
+	go func() {
+		fmt.Println(time.Now())
+
+		for index, user := range users {
+			fmt.Println("Sending user", index)
+			fmt.Println(user.Name, "is", user.Age, "years old")
+
+			// Send each user to the channel
+			ch <- user
+
+			fmt.Println("Sent.✅")
+		}
+	}()
+
+	// Recieving from the channel
+	v := <-ch
+	i := <-ch
+
+	fmt.Println("Validated Users :", v.Name, "and", i.Name)
+
+	fmt.Println("Extitng concurrent program...")
+}
